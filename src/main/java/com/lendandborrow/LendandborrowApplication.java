a2995e69547937af7593db3df08a38f81f4164b1
StefanA <stefanachmueller@hotmail.com>
2021-11-21 14:02:59 +0100

    init with spring

---
+++ src/main/java/com/lendandborrow/LendandborrowApplication.java
@@ -0,0 +1,13 @@
+package com.lendandborrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendandborrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendandborrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lendandborrow/LendandborrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendandborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendandborrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
