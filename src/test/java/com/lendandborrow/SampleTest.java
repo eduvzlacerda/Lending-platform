ca8908cbeb10e341aca43a490a453bd26c8e1276
StefanA <stefanachmueller@hotmail.com>
2021-12-04 09:59:44 +0100

    Setup gitlab pipeline

---
+++ src/test/java/com/lendandborrow/SampleTest.java
@@ -0,0 +1,17 @@
+package com.lendandborrow;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test() {
+
+        Assertions.assertEquals(2, 1 + 1);
+
+    }
+
+
+}
