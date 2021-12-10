4e503342afde091dd818498e9534c47679c000e0
Leon Erd <leon.erd@student.uibk.ac.at>
2021-12-10 23:00:58 +0100

    add search for articles

--- src/main/java/com/lendandborrow/controller/ArticleController.java
+++ src/main/java/com/lendandborrow/controller/ArticleController.java
@@ -3,14 +3,17 @@ package com.lendandborrow.controller;
 import com.lendandborrow.model.Article;
 import com.lendandborrow.model.User;
 import com.lendandborrow.model.dto.ArticleDTO;
+import com.lendandborrow.repositories.ArticleRepository;
 import com.lendandborrow.service.ArticleService;
 import com.lendandborrow.service.UserService;
+import com.lendandborrow.utils.converters.ArticleConverter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.lendandborrow.utils.converters.ArticleConverter.convertArticleDTOToArticle;
 import static com.lendandborrow.utils.converters.ArticleConverter.convertArticleToArticleDTO;
@@ -24,22 +27,34 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final UserService userService;
+    private final ArticleRepository articleRepository;
 
     @GetMapping
     public ResponseEntity<List<ArticleDTO>> getArticles() {
         return ok(articleService.findAllArticles());
     }
 
+    @GetMapping("/search")
+    public ResponseEntity<List<ArticleDTO>> searchArticles(@RequestParam(required = false) String searchString) {
+        if (searchString == null) {
+            return getArticles();
+        }
+        return ok(articleRepository.findBySearch(searchString)
+                .stream()
+                .map(ArticleConverter::convertArticleToArticleDTO)
+                .collect(Collectors.toList()));
+    }
+
     @PostMapping
     public ResponseEntity<ArticleDTO> addArticle(@RequestBody ArticleDTO articleDTO) {
 
         User user = userService.getUser(articleDTO.getUserId());
-        if(user == null){
+        if (user == null) {
             return new ResponseEntity<>(articleDTO, HttpStatus.NOT_FOUND);
         }
-        Article article = convertArticleDTOToArticle(articleDTO,user);
+        Article article = convertArticleDTOToArticle(articleDTO, user);
 
-      return ok(convertArticleToArticleDTO(articleService.addArticle(article,user)));
+        return ok(convertArticleToArticleDTO(articleService.addArticle(article, user)));
 
     }
 
--- src/main/java/com/lendandborrow/repositories/ArticleRepository.java
+++ src/main/java/com/lendandborrow/repositories/ArticleRepository.java
@@ -2,8 +2,17 @@ package com.lendandborrow.repositories;
 
 import com.lendandborrow.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query(
+            value = "SELECT a FROM Article a WHERE (lower(a.title) LIKE %:searchString%) AND (a.articleStatus = 'AVAILABLE')"
+    )
+        // No specified Query needed because Spring JPA supports generating queries using the method name
+    List<Article> findBySearch(@Param("searchString") String searchString);
 }
